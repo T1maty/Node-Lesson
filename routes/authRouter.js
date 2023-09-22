5a3c443ffc81040d3c60c5989ae84a3dfd52ba86
T1maty <timamy666@gmail.com>
2023-09-22 20:43:46 +0300

    Add middleware

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,13 +2,14 @@ const Router = require('express');
 const router = new Router()
 const controller = require('../controller/authController')
 const {check} = require('express-validator')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', [
   check('username', "Field cannot be empty").notEmpty(),
     check('password', "Password size is 4 or 10 symbol").isLength({min:4 , max:10})
 ], controller.registration)
 router.post('/login', controller.login)
-router.get('/users', controller.getUsers)
+router.get('/users', authMiddleware, controller.getUsers)
 
 
 module.exports = router
